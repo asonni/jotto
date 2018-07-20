09c80a29c4cf08dfdcc87b59bb9eff7cf5b97ff1
Aladdin Sonni <aladdin.sonni@gmail.com>
2018-07-20 11:42:36 +0200

    Add secretWord action creatore and reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,9 @@
+import axios from 'axios';
 import { getLetterMatchCount } from '../helpers';
 export const actionTypes = {
   CORRECT_GUESS: 'CORRECT_GUESS',
-  GUESS_WORD: 'GUESS_WORD'
+  GUESS_WORD: 'GUESS_WORD',
+  SET_SECRET_WORD: 'SET_SECRET_WORD'
 };
 
 /**
@@ -11,32 +13,25 @@ export const actionTypes = {
  * @param {string} guessedWord - Guessed word.
  * @returns {function} - Redux Thunk function.
  */
-// export const guessWord = guessedWord => (dispatch, getState) => {
-// const secretWord = getState().secretWord;
-// const letterMatchCount = getLetterMatchCount(guessedWord, secretWord);
-// dispatch({
-//   type: actionTypes.GUESS_WORD,
-//   payload: { guessedWord, letterMatchCount }
-// });
-// if (guessedWord === secretWord) {
-//   dispatch({
-//     type: actionTypes.CORRECT_GUESS
-//   });
-// }
-// };
-
-export const guessWord = guessedWord => {
-  return function(dispatch, getState) {
-    const secretWord = getState().secretWord;
-    const letterMatchCount = getLetterMatchCount(guessedWord, secretWord);
+export const guessWord = guessedWord => (dispatch, getState) => {
+  const secretWord = getState().secretWord;
+  const letterMatchCount = getLetterMatchCount(guessedWord, secretWord);
+  dispatch({
+    type: actionTypes.GUESS_WORD,
+    payload: { guessedWord, letterMatchCount }
+  });
+  if (guessedWord === secretWord) {
     dispatch({
-      type: actionTypes.GUESS_WORD,
-      payload: { guessedWord, letterMatchCount }
+      type: actionTypes.CORRECT_GUESS
     });
-    if (guessedWord === secretWord) {
-      dispatch({
-        type: actionTypes.CORRECT_GUESS
-      });
-    }
-  };
+  }
+};
+
+export const getSecretWord = () => dispatch => {
+  return axios.get('http://localhost:3030').then(response => {
+    dispatch({
+      type: actionTypes.SET_SECRET_WORD,
+      payload: response.data
+    });
+  });
 };
